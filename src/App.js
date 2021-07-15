59b2f68053ce4f92dd19b18c3943edd6a070ee7a
Caner Kuncu <warlordsw@gmail.com>
2021-07-15 22:46:12 +0300

    pass AppTemp to main App.js

--- src/App.js
+++ src/App.js
@@ -1,122 +1,395 @@
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+const initialState = {
+  problem: '',
+  solution: '',
+  key_metrics: '',
+  unique_value: '',
+  unfair: '',
+  channels: '',
+  customer_seg: '',
+  cost_str: '',
+  revenue: '',
+}
+
+const App = () => {
+  // const [list, setList] = useState(initialState)
+  // console.log(list.problem)
+  const [data, setData] = useState(initialState)
+  const [problem, setProblem] = useState([])
+  const [solution, setSolution] = useState([])
+  const [key_metrics, setKey_metrics] = useState([])
+  const [unique_value, setUnique_value] = useState([])
+  const [unfair, setUnfair] = useState([])
+  const [channels, setChannels] = useState([])
+  const [customer_seg, setCustomer_seg] = useState([])
+  const [cost_str, setCost_str] = useState([])
+  const [revenue, setRevenue] = useState([])
+
+  //console.log(data.problem)
+  console.log(solution)
+  const handleProblem = (e) => {
+    e.preventDefault()
+    if (!data.problem) {
+      return
+    } else {
+      const newProblem = {
+        data: data.problem,
+      }
+      setProblem([...problem, newProblem])
+      setData(initialState)
+    }
+  }
+
+  const handleSolution = (e) => {
+    e.preventDefault()
+    if (!data.solution) {
+      return
+    } else {
+      const newSolution = {
+        data: data.solution,
+      }
+      setSolution([...solution, newSolution])
+      setData(initialState)
+    }
+  }
+
+  const handleKeyMetrics = (e) => {
+    e.preventDefault()
+    if (!data.key_metrics) {
+      return
+    } else {
+      const newKeyMetrics = {
+        data: data.key_metrics,
+      }
+      setKey_metrics([...key_metrics, newKeyMetrics])
+      setData(initialState)
+    }
+  }
+
+  const handleUniqueValue = (e) => {
+    e.preventDefault()
+    if (!data.unique_value) {
+      return
+    } else {
+      const newUniqueValue = {
+        data: data.unique_value,
+      }
+      setUnique_value([...unique_value, newUniqueValue])
+      setData(initialState)
+    }
+  }
+
+  const handleUnfair = (e) => {
+    e.preventDefault()
+    if (!data.unfair) {
+      return
+    } else {
+      const newUnfair = {
+        data: data.unfair,
+      }
+      setUnfair([...unfair, newUnfair])
+      setData(initialState)
+    }
+  }
+
+  const handleChannels = (e) => {
+    e.preventDefault()
+    if (!data.channels) {
+      return
+    } else {
+      const newChannels = {
+        data: data.channels,
+      }
+      setChannels([...channels, newChannels])
+      setData(initialState)
+    }
+  }
+
+  const handleCustomerSeg = (e) => {
+    e.preventDefault()
+    if (!data.customer_seg) {
+      return
+    } else {
+      const newCustomerSeg = {
+        data: data.customer_seg,
+      }
+      setCustomer_seg([...customer_seg, newCustomerSeg])
+      setData(initialState)
+    }
+  }
+
+  const handleCostStr = (e) => {
+    e.preventDefault()
+    if (!data.cost_str) {
+      return
+    } else {
+      const newCostStr = {
+        data: data.cost_str,
+      }
+      setCost_str([...cost_str, newCostStr])
+      setData(initialState)
+    }
+  }
+
+  const handleRevenue = (e) => {
+    e.preventDefault()
+    if (!data.revenue) {
+      return
+    } else {
+      const newRevenue = {
+        data: data.revenue,
+      }
+      setRevenue([...revenue, newRevenue])
+      setData(initialState)
+    }
+  }
+
   return (
     <div>
       <div className='flex justify-center'>
-        <div className=' flex flex-col w-96'>
+        <form
+          onSubmit={handleProblem}
+          className=' flex flex-col w-full addHeight'
+        >
           <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 1
+            PROBLEM
           </div>
-          <div className=' bg-base-200  h-96 overflow-y-auto mx-2'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+          <div className=' bg-base-200 mx-2 h-full overflow-y-auto'>
+            {problem.map((item, key) => {
+              return (
+                <div key={key} className='m-2 p-3 bg-base-300 '>
+                  {item.data}
+                </div>
+              )
+            })}
             <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+              <input
+                value={data.problem}
+                onChange={(e) => setData({ ...data, problem: e.target.value })}
+                className='btn w-full'
+                placeholder='+'
+              ></input>
             </div>
           </div>
           <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
-          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 2
-          </div>
-          <div className='bg-base-200 h-64  mx-2  overflow-y-auto'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+        </form>
+        <div className='flex flex-col w-full'>
+          <form
+            onSubmit={handleSolution}
+            className='flex flex-col addHeightTwo'
+          >
+            <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+              SOLUTION
             </div>
-          </div>
-          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+            <div className=' bg-base-200 mx-2 overflow-y-auto h-full'>
+              {solution.map((item, key) => {
+                return (
+                  <div key={key} className='m-2 p-3 bg-base-300 '>
+                    {item.data}
+                  </div>
+                )
+              })}
+              <div className='m-2 bg-base-300'>
+                <input
+                  value={data.solution}
+                  onChange={(e) =>
+                    setData({ ...data, solution: e.target.value })
+                  }
+                  className='btn w-full'
+                  placeholder='+'
+                ></input>
+              </div>
+            </div>
+            <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          </form>
+          <form
+            onSubmit={handleKeyMetrics}
+            className='flex flex-col addHeightTwo'
+          >
+            <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+              KEY METRICS
+            </div>
+            <div className='bg-base-200 mx-2 overflow-y-auto h-full'>
+              {key_metrics.map((item, key) => {
+                return (
+                  <div key={key} className='m-2 p-3 bg-base-300 '>
+                    {item.data}
+                  </div>
+                )
+              })}
+              <div className='m-2 bg-base-300'>
+                <input
+                  value={data.key_metrics}
+                  onChange={(e) =>
+                    setData({ ...data, key_metrics: e.target.value })
+                  }
+                  className='btn w-full'
+                  placeholder='+'
+                ></input>
+              </div>
+            </div>
+            <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          </form>
         </div>
-        <div className='flex flex-col w-96'>
+        <form
+          onSubmit={handleUniqueValue}
+          className='flex flex-col w-full addHeight'
+        >
           <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 3
+            UNIQUE VALUE PROPOSITION
           </div>
-          <div className=' bg-base-200 mx-2  h-96 overflow-y-auto'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+          <div className='bg-base-200 h-full mx-2 overflow-y-auto'>
+            {unique_value.map((item, key) => {
+              return (
+                <div key={key} className='m-2 p-3 bg-base-300 '>
+                  {item.data}
+                </div>
+              )
+            })}
             <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+              <input
+                value={data.unique_value}
+                onChange={(e) =>
+                  setData({ ...data, unique_value: e.target.value })
+                }
+                className='btn w-full'
+                placeholder='+'
+              ></input>
             </div>
           </div>
           <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
-          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 4
-          </div>
-          <div className='bg-base-200 h-64 mx-2  overflow-y-auto'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+        </form>
+        <div className='flex flex-col w-full '>
+          <form onSubmit={handleUnfair} className='flex flex-col addHeightTwo'>
+            <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+              UNFAIR ADVANTAGE
             </div>
-          </div>
-          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+            <div className='bg-base-200 h-full mx-2 overflow-y-auto'>
+              {unfair.map((item, key) => {
+                return (
+                  <div key={key} className='m-2 p-3 bg-base-300 '>
+                    {item.data}
+                  </div>
+                )
+              })}
+              <div className='m-2 bg-base-300'>
+                <input
+                  value={data.unfair}
+                  onChange={(e) => setData({ ...data, unfair: e.target.value })}
+                  className='btn w-full'
+                  placeholder='+'
+                ></input>
+              </div>
+            </div>
+            <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          </form>
+          <form
+            onSubmit={handleChannels}
+            className='flex flex-col addHeightTwo'
+          >
+            <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+              CHANNELS
+            </div>
+            <div className=' bg-base-200 mx-2 h-full overflow-y-auto'>
+              {channels.map((item, key) => {
+                return (
+                  <div key={key} className='m-2 p-3 bg-base-300 '>
+                    {item.data}
+                  </div>
+                )
+              })}
+              <div className='m-2 bg-base-300'>
+                <input
+                  value={data.channels}
+                  onChange={(e) =>
+                    setData({ ...data, channels: e.target.value })
+                  }
+                  className='btn w-full'
+                  placeholder='+'
+                ></input>
+              </div>
+            </div>
+            <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          </form>
         </div>
-        <div className='flex flex-col w-96'>
+        <form
+          onSubmit={handleCustomerSeg}
+          className='flex flex-col w-full addHeight'
+        >
           <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 5
+            CUSTOMER SEGMENTS
           </div>
-          <div className='bg-base-200 h-64 mx-2 overflow-y-auto'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+          <div className='bg-base-200 h-full mx-2 overflow-y-auto'>
+            {customer_seg.map((item, key) => {
+              return (
+                <div key={key} className='m-2 p-3 bg-base-300 '>
+                  {item.data}
+                </div>
+              )
+            })}
             <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+              <input
+                value={data.customer_seg}
+                onChange={(e) =>
+                  setData({ ...data, customer_seg: e.target.value })
+                }
+                className='btn w-full'
+                placeholder='+'
+              ></input>
             </div>
           </div>
           <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+        </form>
+      </div>
+      {/* Alt Sekme Geçiş */}
+      <div className='flex justify-center'>
+        <form onSubmit={handleCostStr} className=' flex flex-col w-full h-60 '>
           <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
-            TOPIC 6
+            COST STRUCTURE
           </div>
-          <div className=' bg-base-200 mx-2 h-96 overflow-y-auto'>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
-            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
-            <div className='m-2 p-3 bg-base-300 '>Tea</div>
-            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+          <div className=' bg-base-200 h-full overflow-y-auto mx-2'>
+            {cost_str.map((item, key) => {
+              return (
+                <div key={key} className='m-2 p-3 bg-base-300 '>
+                  {item.data}
+                </div>
+              )
+            })}
             <div className='m-2 bg-base-300'>
-              <button class='btn w-full'>+</button>
+              <input
+                value={data.cost_str}
+                onChange={(e) => setData({ ...data, cost_str: e.target.value })}
+                className='btn w-full'
+                placeholder='+'
+              ></input>
             </div>
           </div>
           <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
-        </div>
+        </form>
+        <form onSubmit={handleRevenue} className='flex flex-col w-full h-60'>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            REVENUE STREAMS
+          </div>
+          <div className=' bg-base-200 mx-2  h-full overflow-y-auto'>
+            {revenue.map((item, key) => {
+              return (
+                <div key={key} className='m-2 p-3 bg-base-300 '>
+                  {item.data}
+                </div>
+              )
+            })}
+            <div className='m-2 bg-base-300'>
+              <input
+                value={data.revenue}
+                onChange={(e) => setData({ ...data, revenue: e.target.value })}
+                className='btn w-full'
+                placeholder='+'
+              ></input>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+        </form>
       </div>
     </div>
   )
---
+++ src/AppTemp.js
@@ -0,0 +1,123 @@
+function AppTemp() {
+  return (
+    <div>
+      <div className='flex justify-center'>
+        <div className=' flex flex-col w-96'>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 1
+          </div>
+          <div className=' bg-base-200  h-96 overflow-y-auto mx-2'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 2
+          </div>
+          <div className='bg-base-200 h-64  mx-2  overflow-y-auto'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+        </div>
+        <div className='flex flex-col w-96'>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 3
+          </div>
+          <div className=' bg-base-200 mx-2  h-96 overflow-y-auto'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 4
+          </div>
+          <div className='bg-base-200 h-64 mx-2  overflow-y-auto'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+        </div>
+        <div className='flex flex-col w-96'>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 5
+          </div>
+          <div className='bg-base-200 h-64 mx-2 overflow-y-auto'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+          <div className='text-xl p-3 mx-2 bg-base-200 text-center'>
+            TOPIC 6
+          </div>
+          <div className=' bg-base-200 mx-2 h-96 overflow-y-auto'>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 p-3 bg-base-300 '>Coffee</div>
+            <div className='m-2 p-3 bg-base-300 '>Tea</div>
+            <div className='m-2 p-3 bg-base-300 '>Cola</div>
+            <div className='m-2 bg-base-300'>
+              <button class='btn w-full'>+</button>
+            </div>
+          </div>
+          <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppTemp
