6a7e489014e472a41e86e48d7879f5a774508006
Caner Kuncu <warlordsw@gmail.com>
2021-07-20 02:40:11 +0300

    del firestore comments

--- src/context/action.js
+++ src/context/action.js
@@ -4,21 +4,7 @@ let isCreated = false
 
 export const newDataIdNumber = async (dispatch, firebase) => {
   if (isCreated === false) {
-    //const newData = await firebase.firestore().collection('Canvas').doc()
     const newDataRealTime = await firebase.database().ref().push()
-    // const newDataId = newData.id
-    // await newData.set({
-    //   problem: [],
-    //   solution: [],
-    //   key_metrics: [],
-    //   unique_value: [],
-    //   unfair: [],
-    //   channels: [],
-    //   customer_seg: [],
-    //   cost_str: [],
-    //   revenue: [],
-    // })
-
     await newDataRealTime.set({
       problem: '',
       solution: '',
