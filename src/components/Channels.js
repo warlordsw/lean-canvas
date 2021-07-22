32f7779673f93a9aa470a9ad8d1c2a5ad3368c9d
Caner Kuncu <warlordsw@gmail.com>
2021-07-22 15:57:59 +0300

    testing edit button

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import FirebaseContext from '../context/firebase'
-
 const Channels = ({
   channels,
   setChannels,
@@ -13,6 +12,12 @@ const Channels = ({
   baseCanvas,
   setBaseCanvas,
 }) => {
+  const [isEditing, setIsEditing] = useState({
+    editing: false,
+    editButtonActive: false,
+  })
+  const [inputData, setInputData] = useState('')
+
   const canvasData = useContext(FirebaseContext)
 
   const handleChannels = async (e) => {
@@ -50,65 +55,117 @@ const Channels = ({
     }
   }
 
+  const editChannelsButton = (id) => {
+    setIsEditing({ editing: true, editButtonActive: true })
+    const specificChannel = channels.find((prop) => prop.id === id)
+    setInputData(specificChannel.data)
+  }
+
+  const channelsEditFinished = async () => {
+    setIsEditing({ editing: false, editButtonActive: false })
+  }
+
   return (
-    <form onSubmit={handleChannels} className='flex flex-col addHeightTwo'>
+    <div className='flex flex-col addHeightTwo'>
       <div className='text-xl p-3 mx-2 bg-base-200 text-center'>CHANNELS</div>
       <div className=' bg-base-200 mx-2 h-full overflow-y-auto'>
         {channels.map((item, key) => {
           return (
             <div key={key} className='bg-base-300 deldiv'>
-              <div className='flex flex-grow'>{item.data}</div>
-              <button
-                type='button'
-                onClick={() => delSpecificChannels(item.id)}
-                className='btn btn-circle btn-outline btn-xs '
-              >
-                <svg
-                  id='i-edit'
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 32 32'
-                  fill='#ffffff'
-                  stroke='#000000'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
+              {isEditing.editing ? (
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    channelsEditFinished()
+                  }}
                 >
-                  <path d='M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z' />
-                </svg>
-              </button>
-              <button
-                type='button'
-                onClick={() => delSpecificChannels(item.id)}
-                className='btn btn-outline btn-circle btn-xs ml-1'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  className='inline-block w-4 h-4 stroke-current'
+                  <input
+                    className='text-black'
+                    value={inputData}
+                    placeholder='+'
+                    onChange={(e) => setInputData(e.target.value)}
+                  />
+                </form>
+              ) : (
+                <div className='flex flex-grow'>{item.data}</div>
+              )}
+              {isEditing.editButtonActive ? (
+                <button
+                  type='button'
+                  onClick={() => delSpecificChannels(item.id)}
+                  className='btn btn-outline btn-circle btn-xs ml-1'
                 >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M6 18L18 6M6 6l12 12'
-                  ></path>
-                </svg>
-              </button>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    className='inline-block w-4 h-4 stroke-current'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M6 18L18 6M6 6l12 12'
+                    ></path>
+                  </svg>
+                </button>
+              ) : (
+                <div className='flex items-center'>
+                  <button
+                    type='button'
+                    onClick={() => editChannelsButton(item.id)}
+                    className='btn btn-circle btn-outline btn-xs '
+                  >
+                    <svg
+                      id='i-edit'
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 32 32'
+                      fill='#ffffff'
+                      stroke='#000000'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                    >
+                      <path d='M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z' />
+                    </svg>
+                  </button>
+                  <button
+                    type='button'
+                    onClick={() => delSpecificChannels(item.id)}
+                    className='btn btn-outline btn-circle btn-xs ml-1'
+                  >
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      className='inline-block w-4 h-4 stroke-current'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M6 18L18 6M6 6l12 12'
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              )}
             </div>
           )
         })}
         <div className='m-2 bg-base-300'>
-          <input
-            value={data.channels}
-            onChange={(e) => setData({ ...data, channels: e.target.value })}
-            className='btn w-full'
-            placeholder='+'
-          ></input>
+          <form onSubmit={handleChannels}>
+            <input
+              value={data.channels}
+              onChange={(e) => setData({ ...data, channels: e.target.value })}
+              className='btn w-full'
+              placeholder='+'
+            ></input>
+          </form>
         </div>
       </div>
       <div className='text-xl p-3 mx-2 mb-2 h-12 bg-base-200 text-center'></div>
-    </form>
+    </div>
   )
 }
 
